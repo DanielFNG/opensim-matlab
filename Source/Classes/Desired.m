47a9bbd7870bd5eaf80dfcdf23b392ed5970af44
Daniel Gordon <daniel.gordon@ed.ac.uk>
2017-04-12 11:32:58 +0100

    Argument parser for percentage reduction desired
    Implemented and did some testing of argument list parser for setting up
    percentage reduction desireds.

--- Source/Classes/Desired.m
+++ Source/Classes/Desired.m
@@ -7,7 +7,7 @@ classdef Desired
     end
     
     properties (SetAccess = private, GetAccess = private)
-        varargin
+        varargin % Column cell array!
     end
     
     methods
@@ -22,16 +22,53 @@ classdef Desired
         function obj = setupPercentageReduction(obj,IDTrial)
             % Here it is assumed that varargin = [joints, mutliplier].
             % If joints is a string, 'all', it is assumed all joints are to
-            % be multiplied. If joints is an array of joint identifiers
+            % be multiplied. If joints is a cell array of joint identifiers
             % i.e. 'r_hip_f' then those identified are the ones to be
             % multiplied. Multiplier can either be a constant, meaning all
             % DOFS are multiplied by the same value, OR an array of the
             % same size as joints (or an n dimensional array if joints ==
             % 'all') meaning there's a 1:1 correspondence of
             % joint-multiplier.
+            
+            if size(obj.varargin,2) ~= 2
+                error(['Need two input arguments to desired for '...
+                    'percentage reduction.']);
+            end
+            
+            % First get the number of DOFs from the data. 
+            nDofs = size(IDTrial.Labels,2) - 1; % -1 to remove the time col
+            
+            [identifiers, multipliers] = ...
+                obj.parsePercentageReductionArguments(IDTrial);
+                    
         end
         
-        function obj = setupMatchTrajectory(obj)
+        function [identifiers, multipliers] = ...
+                parsePercentageReductionArguments(obj, IDTrial)
+            if isa(obj.varargin{1}, 'char') && strcmp(obj.varargin{1}, 'all')
+                identifiers = IDTrial.Labels(2:end);
+                if size(obj.varargin{2},2) == 1
+                    multipliers = obj.varargin{2}*ones(1,nDofs);
+                elseif size(obj.varargin{2},2) < nDofs
+                    error(['Attempting percentage reduction across all' ...
+                        ' joints, but received fixed number of multipliers' ...
+                        ' less than total number of joints.']);
+                elseif size(obj.varargin{2},2) == nDofs
+                    multipliers = obj.varargin{2};
+                else
+                    error('Received more multipliers than model DOFS.');
+                end
+            elseif size(obj.varargin{1},2) ~= size(obj.varargin{2},2)
+                error(['Attempting percentage reduction at subset of '...
+                    'joints, but was given an unmatching number of '...
+                    'multipliers.']);
+            else
+                identifiers = obj.varargin{1};
+                multipliers = obj.varargin{2};
+            end
+        end
+        
+        function obj = setupMatchTrajectory(obj,IDTrial)
         end
         
         function obj = evaluateDesired(obj, IDTrial)
