9b8a1a231c651837e517c34d233cf3d5ffb5ce1e
Daniel Gordon <daniel.gordon@ed.ac.uk>
2017-04-26 15:27:57 +0100

    ContactForceSet, adding functionality to LinearExoForceModel
    - Implemented a ContactForceSet which stores spatial forces. Also, made
    it so that the LinearExoskeletonForceModel has access to the
    FrameJacobianSet and ContactForceSet calculated during its construction.
    
    - Adding functionality to linearExoskeletonForceModel. Since it has
    access to a ContactForceSet and FrameJacobianSet now, it can calculate
    the spatial forces at the contact points given known trajectories (for
    example). For the moment I've added support only for providing some
    sample trajectories, which are stretched to the right size and used as
    the exoskeleton motor torques in order to calculate the spatial forces.
    This method could probably do with some work and generality though - it
    seems like the ContactForceSet should also have knowledge of which motor
    is 'linked' to each contact point.

--- Source/Classes/LinearExoskeletonForceModel.m
+++ Source/Classes/LinearExoskeletonForceModel.m
@@ -10,16 +10,19 @@ classdef LinearExoskeletonForceModel
     % models for specific Exoskeletons given a unique identifier (their
     % name). 
     
-    properties
+    properties (SetAccess = private)
         Exoskeleton % the exoskeleton to which this model relates
-        States % time-indexed states of the human-exoskeleton model 
+        States % time-indexed states of the human-exoskeleton model
         P % cell array (even if only one element!) containing linear mult's.
-        Q % cell array containing linear constants. 
+        Q % cell array containing linear constants.
+        FrameJacobians % FrameJacobianSet 
+        ContactForces % ContactForceSet 
     end
-    
+        
     methods
         
-        function obj = LinearExoskeletonForceModel(exo, states, P, Q)
+        function obj = LinearExoskeletonForceModel(exo, states, P, Q, ...
+                Jacobians, Forces)
             % Name is an identifier and P, Q are cell arrays containing
             % indexed P, Q. Note: even if this is for a single timestep
             % i.e. working with only one P and one Q, still have to be
@@ -32,8 +35,53 @@ classdef LinearExoskeletonForceModel
                 obj.States = states;
                 obj.P = P;
                 obj.Q = Q;
+                obj.FrameJacobians = Jacobians;
+                obj.ContactForces = Forces;
             end
         end    
+        
+        function spatialForcesSet = ...
+                calculateSpatialForcesFromTorqueTrajectory(obj, MotorTorques)
+            % MotorTorques should be an array with nColumns equal to the number
+            % of motors of the exoskeleton, and an arbitrary number of 
+            % rows. The trajectory defined by each column is interpolated to 
+            % be the same size as the ContactForces (i.e. the numer of 
+            % timesteps in States). So it will be more accurate with more rows.
+            
+            % Check that you have a trajectory for each motor.
+            nTrajectories = size(MotorTorques,2);
+            if nTrajectories ~= obj.Exoskeleton.Exo_dofs
+                error('Size error.');
+            end
+            
+            % Rescale the trajectories.
+            desiredRows = size(obj.States.Timesteps,1);
+            scaledMotorTorques = zeros(desiredRows,nTrajectories); % preallocate
+            for i=1:nTrajectories
+                scaledMotorTorques(1:end,i) = stretchVector(...
+                    MotorTorques(1:end,i), desiredRows);
+            end
+            
+            % Calculate the spatial forces resulting from the scaled motor
+            % torque trajectories.
+            spatialForces{desiredRows, nTrajectories} = 0; % preallocate
+            for i=1:desiredRows
+                spatialForces{i,1} = ... 
+                    scaledMotorTorques(i,1) ... % right motor 
+                    * obj.ContactForces.ForceSet{i,1};
+                spatialForces{i,2} = ...
+                    scaledMotorTorques(i,2) ... % left motor 
+                    * obj.ContactForces.ForceSet{i,2};
+                spatialForces{i,3} = ...
+                    scaledMotorTorques(i,1) ...
+                    * obj.ContactForces.ForceSet{i,3};
+                spatialForces{i,4} = ...
+                    scaledMotorTorques(i,2) ...
+                    * obj.ContactForces.ForceSet{i,4};
+            end
+            spatialForcesSet = ContactForceSet(obj.FrameJacobians, ...
+                spatialForces, scaledMotorTorques);
+        end
         
     end
     
---
+++ Source/Common/scaleVectorToDesiredSize.m
@@ -0,0 +1,8 @@
+function scaled_vector = scaleVectorToDesiredSize(input_vector, desired_size)
+% Given a vector of so many elements, stretch/compress it to a specified
+% desired size. Inputs should be vectors so Nx1 arrays.
+    x = 1:1:size(input_vector,1);
+    z = 1:1:desired_size;
+    scaled_vector = interp1(x, input_vector, z);
+end
+
