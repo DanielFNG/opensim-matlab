bd5cdc738042cc06d8a651390c135527b3ab7cd0
Graham <grahamhendo@gmail.com>
2017-06-02 17:19:12 +0100

    Calculates the step width
    Needs to be tested.

--- Source/Scripts/Exoskeleton gait metric analysis/calculateMetricStepWidth.m
+++ Source/Scripts/Exoskeleton gait metric analysis/calculateMetricStepWidth.m
@@ -1,55 +1,84 @@
 
+% root = 'C:\Users\Graham\Documents\MATLAB\MOtoNMS_v2_2\MyData\ElaboratedData';
+% 
+% load ([root 'IK_Results.mat'])
 
 % Loop over the nine subjects. 
 for subject=1:8
     % Skip the missing data. 
     if ~ (subject == 5)
-        % There are four dates which need to be represented in the path.
-
-        % Loop over left/right gait cycles. 
-        for j=1:2
-            switch j
-                case 1
-                    gait = [subject_path '\dynamicElaborations\right'];
-                case 2
-                    gait = [subject_path '\dynamicElaborations\left'];
-            end
+        
+        % Loop over the three assistance levels. 
+        for assistance_level=1:3
             
             % Loop over the ten contexts. 
             for i=1:10  
-                % Filenames are different for steady state vs non steady state.
-                if mod(i,2) == 1
-                    folder = [gait 'Non-StSt'];
-                else
-                    folder = [gait 'StSt'];
-                end
                 
-                for assistance_level=1:3
-                    if assistance_level == 1
-                        % No APO.
-                        ik_folder = [folder '\NE' num2str(i)];
-                        model = human_model;
-                    elseif assistance_level == 2
-                        % With APO, transparent.
-                        ik_folder = [folder '\ET' num2str(i)];
-                        model = APO_model;
-                    elseif assistance_level == 3
-                        % With APO, assisted. 
-                        ik_folder = [folder '\EA' num2str(i)];
-                        model = APO_model;
-                    end
-                    [IK_array{subject,assistance_level,j,i}, ...
-                    Input_Markers_array{subject,assistance_level,j,i},...
-                    Output_Markers_array{subject,assistance_level,j,i}] ...
-                        = runBatchIK(model, ik_folder, [ik_folder '\IK_Results']);
-                    if mod(i,2) == 1
-                        current_ik = current_ik + 2;
+                if i == 1 || i == 2 || i == 4 || i == 6 || i == 8
+                    
+                    % Loop over the five gait cycles for the relevant
+                    % contexts
+                    
+                    % Work out which step comes first
+                    first = getFirstHeelStrike(Input_Markers_array{subject,assistance_level,1,i}{1}...
+                    ,Input_Markers_array{subject,assistance_level,2,i}{1});
+                    
+                    if first == 1
+                        for k = 1:4
+                        % for the case where right is the first heelstrike
+                        Step_Width_array{subject,assistance_level,2,i}{k} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+
+                        Step_Width_array{subject,assistance_level,1,i}{k} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k+1},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+                        
+                        end
+                    
                     else
-                        current_ik = current_ik + 5;
+                        for k = 1:4
+                         % for the case where left is the first heelstrike
+                        Step_Width_array{subject,assistance_level,1,i}{k} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+
+                        Step_Width_array{subject,assistance_level,2,i}{k} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k+1}); %Send both L and R IK files to calculate step width function}
+                
+                        end            
                     end
-                    waitbar(current_ik/total_iks);
-                end
+                    
+                else
+                    
+                    % Loop over the two gait cycles for the relevant
+                    % contexts
+                    if first == 1
+                        % for the case where right is the first heelstrike
+                        k = 1;
+                        Step_Width_array{subject,assistance_level,2,i}{1} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+
+                        Step_Width_array{subject,assistance_level,1,i}{1} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k+1},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+                        
+                        Step_Width_array{subject,assistance_level,2,i}{2} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k+1},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k+1}); %Send both L and R IK files to calculate step width function}
+                   
+                    
+                    else
+                        k = 1;
+                        % for the case where left is the first heelstrike
+                        Step_Width_array{subject,assistance_level,1,i}{1} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k}); %Send both L and R IK files to calculate step width function}
+
+                        Step_Width_array{subject,assistance_level,2,i}{1} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k+1}); %Send both L and R IK files to calculate step width function}
+                        
+                        Step_Width_array{subject,assistance_level,1,i}{2} = getStepWidth(Input_Markers_array{subject,assistance_level,1,i}{k+1},...
+                        Input_Markers_array{subject,assistance_level,2,i}{k+1}); %Send both L and R IK files to calculate step width function}
+                
+                           
+                    end
+                end                    
             end
-        end
+        end      
     end
 end
