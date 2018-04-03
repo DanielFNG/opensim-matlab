b3fd8a4b58142c791a6841949937d9ebfe172e25
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-04-03 15:23:47 +0100

    Change to offset calculation

--- Source/Scripts/calculateOffsets.m
+++ Source/Scripts/calculateOffsets.m
@@ -1,11 +1,14 @@
 subjects = [1:4, 6:8];
 
+x_offset = 0.2081*1000;
+y_offset = -0.0953*1000;
+
 for subject = subjects
     static = Data(['Static' num2str(subject) '.trc']);
-    APO_x = static.getDataCorrespondingToLabel('V_SacralX') + 0.2081;
-    APO_y = static.getDataCorrespondingToLabel('V_SacralY') - 0.0953;
-    offset_R_x = APO_x - static.getDataCorrespondingToLabel('RHJCX');
-    offset_R_y = APO_y - static.getDataCorrespondingToLabel('RHJCY');
-    offset_L_x = APO_x - static.getDataCorrespondingToLabel('LHJCX');
-    offset_L_y = APO_y - static.getDataCorrespondingToLabel('LHJCY');
+    APO_x = static.getDataCorrespondingToLabel('V_SacralX') + x_offset;
+    APO_y = static.getDataCorrespondingToLabel('V_SacralY') + y_offset;
+    offset_R_x = abs(APO_x - static.getDataCorrespondingToLabel('RHJCX'));
+    offset_R_y = abs(APO_y - static.getDataCorrespondingToLabel('RHJCY'));
+    offset_L_x = abs(APO_x - static.getDataCorrespondingToLabel('LHJCX'));
+    offset_L_y = abs(APO_y - static.getDataCorrespondingToLabel('LHJCY'));
 end
