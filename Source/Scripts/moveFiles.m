3ba9c432d377bde9ef4c06c9973dff4699d33ee9
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-04-20 23:24:10 +0100

    Changes to moving scripts

--- Source/Scripts/moveFiles.m
+++ Source/Scripts/moveFiles.m
@@ -2,28 +2,20 @@
 startloc = 'D:\Dropbox\PhD\Exoskeleton Metrics Offsets Axial';
 endloc = 'D:\Dropbox\PhD\Exoskeleton Metrics Compliant';
 
-scale_folder = 'Scaling';
 data_folder = 'dynamicElaborations\right';
 
-subjects = 6:8;
+subjects = [1:4, 6:8];
 contexts = 2:2:10;
 
 for subject = subjects
-    % Copy scaling folders.
-    copyfile([startloc filesep 'S' num2str(subject) filesep scale_folder], [endloc filesep 'S' num2str(subject) filesep scale_folder]);
     
     for context = contexts
-        % Make folder.
-        mkdir([endloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context)])
         
         % Copy GRFs.
         grf_files = dir([startloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context) filesep '*.mot']);
         for i=1:length(grf_files)
             copyfile([startloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context) filesep grf_files(i).name], [endloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context) filesep grf_files(i).name]);
         end
-        
-        % Copy RRA folder. 
-        copyfile([startloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context) filesep 'RRA_Results'], [endloc filesep 'S' num2str(subject) filesep data_folder filesep 'EA' num2str(context) filesep 'RRA_Results']);
     end
 end
 
