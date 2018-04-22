730365bddc099ada74f9b6c19cb4450f6a4c9fc0
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-04-22 18:05:11 +0100

    Incremental update

--- Source/ROBIO 2018 Invariant Metrics/Gait Metrics/metric.m
+++ Source/ROBIO 2018 Invariant Metrics/Gait Metrics/metric.m
@@ -1,6 +1,6 @@
 classdef metric < handle
     
-    properties %(SetAccess = private)
+    properties (SetAccess = private)
         name
         sample_size 
         means  
@@ -8,38 +8,61 @@ classdef metric < handle
         row_diffs
         col_diffs
         n_rows
-        n_columns
+        n_cols
         row_sig_diffs
         col_sig_diffs
-        combined_means
-        combined_sdevs
+        p_value = 0.05
+        row_labels
+        col_labels
     end
         
     properties (SetAccess = private, GetAccess = private)
-        p_value = 0.05
-        assistance_order = {'NE', 'ET', 'EA-I', 'EA-C'}
-        context_order = {'BW','UW','DW','FW','SW'}
+        base_row = 1 % Assume baseline corresponds to (1,1)
+        base_col = 1
+        combined_means
+        combined_sdevs
     end
     
     methods 
         
         function obj = metric(name, means, sdevs, sample_size, ...
-                col_diffs, row_diffs)
+                col_diffs, row_diffs, row_labels, col_labels, baseline)
             if nargin > 0
                 obj.name = name;
-                if nargin == 6
+                if nargin >= 6
                     obj.means = means;
                     obj.sdevs = sdevs;
                     obj.sample_size = sample_size;
                     obj.row_diffs = row_diffs;
                     obj.n_rows = size(row_diffs, 1);
                     obj.col_diffs = col_diffs;
-                    obj.n_columns = size(col_diffs, 1);
+                    obj.n_cols = size(col_diffs, 1);
                     obj = obj.identifySignificantDifferences();
                     obj = obj.calcCombinedMeansAndSdevs();
+                    if nargin >= 8
+                        obj.row_labels = row_labels;
+                        obj.col_labels = col_labels;
+                        if nargin == 9
+                            obj.base_row = baseline(1);
+                            obj.base_col = baseline(2);
+                        end
+                    else
+                        obj.assignDefaultLabels();
+                    end
                 else
-                    error('A non-empty metric requires six arguments.');
+                    error('Non-empty metrics require 6+ arguments.');
                 end
+            end
+        end
+        
+        % Assigns default labelling to the rows and columns e.g. row1, 
+        % col1, etc.
+        function assignDefaultLabels(obj)
+            for i=1:obj.n_rows
+                obj.row_labels{i} = ['row' num2str(i)];
+            end
+            for i=1:obj.n_cols
+                obj.col_labels{i} = ['col' num2str(i)];
             end
         end
         
@@ -47,8 +70,9 @@ classdef metric < handle
         % which combinations of rows/columns exhibit significant differences.
         % For example row_sig_diffs(1,2) = 1 => there is a significant 
         % difference between row 1 and row 2. The default is 0, meaning 
-        % no significant differences. 
-        function obj = identifySignificantDifferences(obj)
+        % no significant differences. The matrix is left as upper
+        % triangular.
+        function identifySignificantDifferences(obj)
             
             obj.row_sig_diffs = zeros(obj.n_rows);
             obj.col_sig_diffs = zeros(obj.n_cols);
@@ -56,106 +80,73 @@ classdef metric < handle
             for i=1:size(obj.col_diffs,1)
                 if obj.col_diffs(i,6) < obj.p_value
                     obj.col_sig_diffs(obj.col_diffs(i, 1), obj.col_diffs(i, 2)) = 1;
-                    obj.col_sig_diffs(obj.col_diffs(i, 2), obj.col_diffs(i, 1)) = 1;
                 end
             end
             
             for i=1:size(obj.row_diffs,1)
                 if obj.row_diffs(i,6) < obj.p_value 
                     obj.row_sig_diffs(obj.row_diffs(i, 1), obj.row_diffs(i, 2)) = 1;
-                    obj.row_sig_diffs(obj.row_diffs(i, 2), obj.row_diffs(i, 1)) = 1;
                 end
             end         
         end
         
         % This calculates obj.combined_means, which is a map from a label
-        % (corresponding to either an assistance level or walking context)
-        % on to the combined mean for that label.
+        % (corresponding to either a row or column) on to the combined 
+        % mean for that label.
         %
-        % For example, obj.combined_means('NE') is the mean value of
-        % (NE,BW), (NE,IW), (NE,DW), ... etc. And likewise for the context
-        % labels. 
+        % For example, obj.combined_means('row1') is the mean value of
+        % (row1,col1), (row2,col2), ... etc. And likewise for the column
+        % labels.
         function calcCombinedMeansAndSdevs(obj)
-            n_assist = size(obj.assistance_order,2);
-            n_context = size(obj.context_order,2);
+            % Preallocate variables. 
+            n_conditions = obj.n_rows + obj.n_cols;
+            keys = cell(1, n_conditions);
+            comb_means = zeros(1, n_conditions);
+            comb_sdevs = zeros(1, n_conditions);
+            
+            % Calculate combined row and column means.
             comb_means(1:obj.n_rows) = mean(obj.means(1:end, :));
-            comb_means(1:obj.n_columns) = mean(obj.means(:, 1:end));
+            comb_means(obj.n_rows + 1:obj.n_rows + obj.n_cols) = ...
+                mean(obj.means(:, 1:end));
             
+            % Calculate combined row sdevs. 
             for i=1:obj.n_rows
-                temp = 0;
-                for j=1:obj.context_order
-                    temp = temp + metric.intermediateVariance(obj.sample_size, obj.sdevs(i, j)^2, obj.means(i, j), comb_means(i));
-                end
-                result = sqrt(temp/(d*obj.sample_size - 1));
-                comb_sdevs = [comb_sdevs result];
+                % Increment assistance/context label for later use in Map.
+                keys{i} = obj.row_labels{i};
+                intRowVars(1:obj.n_cols) = metric.intermediateVariance(obj.sample_size, obj.sdevs(i, 1:end).^2, obj.means(i, 1:end), comb_means(i));
+                comb_sdevs(i) = sqrt(sum(intRowVars)/(obj.n_cols*obj.sample_size - 1));
             end
             
-            for i=1:obj.n_rows
-                intermediateVariances(1:obj.context_order) = metric.intermediateVariance(obj.sample_size, obj.sdevs(i, 1:obj.context_order)^2
+            % Calculate combined column sdevs.
+            for i=1:obj.n_cols
+                keys{i + obj.n_rows} = obj.col_labels{i};
+                intColVars(1:obj.n_rows) = metric.intermediateVariance(obj.sample_size, obj.sdevs(1:end, i).^2, obj.means(1:end, i), comb_means(i + obj.n_rows));
+                comb_sdevs(i + obj.n_rows) = sqrt(sum(intColVars)/(obj.n_rows*obj.sample_size - 1));
             end
-                  
-            for i=1:n_assist
-                keys{i} = obj.assistance_order{1,i};
-                comb_means = [comb_means mean(obj.means(i,1:end))];
-                comb_sdevs = [comb_sdevs obj.calcCombSdevs(...
-                    comb_means(end), {'assistance', i})];
-            end
-            for i=1:n_context
-                keys{i+n_assist} = obj.context_order{1,i};
-                comb_means = [comb_means mean(obj.means(1:end,i))];
-                comb_sdevs = [comb_sdevs obj.calcCombSdevs(...
-                    comb_means(end), {'context', i})];
-            end
-            obj.combined_means = containers.Map(keys,comb_means);
-            obj.combined_sdevs = containers.Map(keys,comb_sdevs);
+            
+            % Create mappings from the condition labels to the combined
+            % means and sdevs. 
+            obj.combined_means = containers.Map(keys, comb_means);
+            obj.combined_sdevs = containers.Map(keys, comb_sdevs);
         end
         
-        function result = calcCombSdevs(obj, overall_mean, indices)
-            if strcmp(indices{1}, 'assistance')
-                assistance_level = indices{2};
-                d = size(obj.context_order,2);
-                temp = 0;
-                for i=1:d
-                    temp = temp + metric.intermediateVariance(...
-                        obj.sample_size, ...
-                        obj.sdevs(assistance_level,i)^2, ... % v = sdev^2
-                        obj.means(assistance_level,i), overall_mean);
-                end
+        % Calculate signed or absolute relative differences.
+        function diff = calculateRelativeDifferences(obj, mode)
+            diff = zeros(size(obj.means));
+            baseline = obj.means(obj.base_row, obj.base_col);
+            if strcmp(mode, 'unsigned')
+                diff(:, :) = 100*(abs(obj.means(:, :) - baseline)/baseline);
+            elseif strcmp(mode, 'signed')
+                diff(:, :) = 100*(obj.means(:, :) - baseline)/baseline;
             else
-                context = indices{2};
-                d = size(obj.assistance_order,2);
-                temp = 0;
-                for i=1:d
-                    temp = temp + metric.intermediateVariance(...
-                        obj.sample_size, obj.sdevs(i,context)^2, ...
-                        obj.means(i,context), overall_mean);
-                end
-            end
-            result = sqrt(temp/(d*obj.sample_size - 1));  % sdev = sqrt(V)
-        end
-        
-        function diff = calculateSignedRelativeDifferences(obj)
-            diff = zeros(size(obj.means));
-            baseline = obj.means(1,1);
-            for i=1:length(obj.assistance_order)
-                for j=1:length(obj.context_order)
-                    diff(i,j) = 100*(obj.means(i,j) - baseline)/baseline;
-                end
+                error('Unrecognised mode.');
             end
         end
         
-        function diff = calculateRelativeDifferences(obj)
-            diff = zeros(size(obj.means));
-            baseline = obj.means(1,1); 
-            for i=1:length(obj.assistance_order)
-                for j=1:length(obj.context_order)
-                    diff(i,j) = 100*(abs(obj.means(i,j) - baseline)/baseline);
-                end
-            end
-        end
-        
+        % Makes sense to use unsigned here to capture the magnitude of the
+        % effect and avoid any cancellation. 
         function overall = calculateOverall(obj)
-            diff = obj.calculateRelativeDifferences();
+            diff = obj.calculateRelativeDifferences('unsigned');
             diff = reshape(diff,1,[]);
             overall = mean(diff);
         end
@@ -165,15 +156,15 @@ classdef metric < handle
         % or context (vice versa). 'direction' should be 'assistance' or 
         % 'context' depending on the mode. 
         function avg_1d = calculate1DAvg(obj, direction)
-            diff = obj.calculateRelativeDifferences();
+            diff = obj.calculateRelativeDifferences('unsigned');
             if strcmp(direction, 'context')
-                avg_1d = 1:length(context_order);
-                for i=1:length(context_order)
+                avg_1d = 1:obj.n_cols;
+                for i=1:obj.n_cols
                     avg_1d(i) = mean(diff(1:end,i));
                 end
             elseif strcmp(direction, 'assistance')
-                avg_1d = 1:length(obj.assistance_order);
-                for i=1:length(obj.assistance_order)
+                avg_1d = 1:obj.n_rows;
+                for i=1:obj.n_rows
                     avg_1d(i) = mean(diff(i,1:end));
                 end
             end
@@ -307,10 +298,10 @@ classdef metric < handle
             % t tests comparing means to baselines
             % 14 effect size results for each metric. Start off with a 
             % 5 x 3 matrix for convenience. 
-            cohens_d = zeros(length(obj.assistance_order),length(obj.context_order));
+            cohens_d = zeros(obj.n_rows,obj.n_cols);
             % over assistance levels and contexts...
-            for i=1:length(obj.context_order)
-                for j=1:length(obj.assistance_order)
+            for i=1:obj.n_cols
+                for j=1:obj.n_rows
                     % Don't compare the baseline to itself.
                     if ~ (i == 1 && j == 1)
                         % Calculate pooled standard deviation.
@@ -322,17 +313,14 @@ classdef metric < handle
             end
         end
         
+        % Mode should be 'absolute', 'signed' or 'unsigned'.
         function plot3DBar(obj, mode)
             
             % Compute relative differences from the baseline. 
             if strcmp(mode, 'absolute')
                 diff = obj.means;
-            elseif strcmp(mode, 'relative_signed')
-                diff = obj.calculateSignedRelativeDifferences;
-            elseif strcmp(mode, 'relative')
-                diff = obj.calculateRelativeDifferences; 
             else
-                error('Mode for plot3DBar not recognised.');
+                diff = obj.calculateRelativeDifferences(mode);
             end
             
             % Set dimensions.
@@ -367,8 +355,8 @@ classdef metric < handle
             zlabel('% difference from baseline', 'FontWeight', 'bold');
             ylabel('Assistance Level', 'FontWeight', 'bold');
             set(ax, 'FontSize', 20, 'FontWeight', 'bold', 'XTick', ...
-                1:length(obj.context_order), 'XTickLabel', obj.context_order, 'YTick', ...
-                1:length(obj.assistance_order), 'YTickLabel', obj.assistance_order);
+                1:obj.n_cols, 'XTickLabel', obj.context_order, 'YTick', ...
+                1:obj.n_rows, 'YTickLabel', obj.assistance_order);
             words = strsplit(obj.name(1:end-2), '_');
             if length(words) > 1
                 for i=1:length(words)
@@ -416,30 +404,16 @@ classdef metric < handle
     
     methods (Static)
         
-        function index = mapLabel(string)
-            if strcmp(string, 'NE') || strcmp(string, 'BW')
-                index = 1;
-            elseif strcmp(string, 'ET') || strcmp(string, 'IW')
-                index = 2;
-            elseif strcmp(string, 'EA') || strcmp(string, 'DW') || strcmp(string, 'EA-I')
-                index = 3;
-            elseif strcmp(string, 'FW') || strcmp(string, 'EA-C')
-                index = 4;
-            elseif strcmp(string, 'SW')
-                index = 5;
-            else
-                error('String not recognised.')
-            end
-        end
-        
         % Function for calculating the intermediate terms when calculating
-        % combined variance of groups. 
+        % combined variance of groups. Supports scalar or vector arguments
+        % for variance, mean and overall mean. Sample size must be a 
+        % scalar. 
         function result = intermediateVariance(...
                 samples, variance, mean, overall_mean)
             result = ((samples - 1) * variance) ...
-                + (samples * mean^2) ...
-                - (2 * samples * mean * overall_mean) ...
-                + (samples * overall_mean^2);
+                + (samples * mean.^2) ...
+                - (2 * samples * mean .* overall_mean) ...
+                + (samples * overall_mean.^2);
         end
         
         % This function calculates Cohen's d for two groups of data, 
