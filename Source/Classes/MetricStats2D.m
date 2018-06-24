83e309d9763e8ddc90769695ac867c04b79bec50
Daniel Gordon <DanielFNG@users.noreply.github.com>
2018-06-24 16:39:35 +0100

    Adding description to MetricStats2D

--- Source/Classes/MetricStats2D.m
+++ Source/Classes/MetricStats2D.m
@@ -1,3 +1,7 @@
+% A class for statistically analysing two-dimensional data.
+% In particular it can be used for walking data indexed by walking 
+% context (speed, incline, etc) and assistance level (humans walking
+% with powered exoskeletons or prostheses). 
 classdef MetricStats2D < handle
     
     properties (SetAccess = private)
