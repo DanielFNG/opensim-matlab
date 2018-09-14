07e9cfd6bec16c9931e5b6e9138a3bf390e1a228
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-09-14 12:30:00 +0100

    Revert "Revert "Begin Data class update""
    This reverts commit ed50e88030744a2a8bf3f5fd67532011798006df.

--- Source/Tools/rel2abs.m
+++ Source/Tools/rel2abs.m
@@ -4,8 +4,6 @@ function varargout = rel2abs(varargin)
 for i=1:length(varargin)
     if ~java.io.File(varargin{i}).isAbsolute
         varargout{i} = fullfile(pwd, varargin{i});
-    else
-        varargout{i} = varargin{i};
     end
 end
     
