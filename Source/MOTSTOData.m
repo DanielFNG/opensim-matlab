c1392048381f831fc1ef204e25ec3e16a49b3949
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-30 22:11:09 +0100

    Begin splitting data class

---
+++ Source/MOTSTOData.m
@@ -0,0 +1,13 @@
+classdef MOTData < OpenSimData
+
+    properties
+        Filetype = '.mot' 
+    end
+    
+    methods
+    
+        function obj = MOTData(file, values, header, labels)
+    
+    end
+
+end
