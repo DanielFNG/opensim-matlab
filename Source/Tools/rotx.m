23de8770fe106eedc3a99733c7576c8c76c356bc
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-31 23:01:32 +0100

    Move more functions to tools

--- Source/Tools/rotx.m
+++
@@ -1,9 +0,0 @@
-function rot = rotx(angle)
-% Return the rotation matrix for a rotation of angle about the x axis.
-
-angle = degtorad(angle);
-rot = [1, 0, 0; ...
-    0, cos(angle), -sin(angle); ...
-    0, sin(angle), cos(angle)];
-
-end
