a9ca98ab3539803bf3e0e35281ff1bcec55bae1d
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-31 16:16:03 +0100

    Move tool to new repo

--- Source/Tools/dirNoDots.m
+++
@@ -1,8 +0,0 @@
-
-% Access files and subdirectories in a directory without '.' or '..' entries.
-function d = dirNoDots(directory)
-
-d = dir(directory);
-d = d(~ismember({d.name}, {'.', '..'}));
-
-end
