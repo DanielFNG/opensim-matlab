4e1387c295ac4af592fcdb5cc055dc35acc298c8
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-02-09 17:41:51 +0000

    Update OpenSimResults.m
    - Make load public in case users need to load an analyis later.
    - Fix how preload works with GRF.

--- Source/OpenSimResults.m
+++ Source/OpenSimResults.m
@@ -29,6 +29,54 @@ classdef OpenSimResults < handle
             obj.load(analyses);
         end
         
+        function load(obj, analyses)
+        % Load the data from each analysis in turn.
+        
+            % Support for a single analysis input as a string. 
+            if isa(analyses, 'char')
+                analyses = {analyses};
+            end
+            
+            for i=1:length(analyses)
+            
+                % Check analysis is computed - if so get results folder.
+                analysis = analyses{i};
+                folder = obj.preload(analysis);
+                obj.(analysis) = {};
+            
+                % Load analysis Data. 
+                switch analysis
+                    case 'GRF'
+                        obj.GRF.Forces = Data(obj.Trial.grfs_path);
+                    case 'IK'
+                        obj.IK.Kinematics = Data([folder filesep 'ik.mot']);
+                        obj.IK.InputMarkers = Data(obj.Trial.input_coordinates);
+                        obj.IK.OutputMarkers = Data(...
+                            [folder filesep 'ik_model_marker_locations.sto']);
+                    case 'RRA'
+                        obj.RRA.Kinematics = ...
+                            Data([folder filesep 'RRA_Kinematics_q.sto']);
+                        obj.RRA.Forces = ...
+                            Data([folder filesep 'RRA_Actuation_force.sto']);
+                        obj.RRA.TrackingErrors = ...
+                            Data([folder filesep 'RRA_Kinematics_q.sto']);
+                    case 'BK'
+                        obj.BK.Positions = Data([folder filesep ...
+                            'Analysis_BodyKinematics_pos_global.sto']);
+                        obj.BK.Velocities = Data([folder filesep ...
+                            'Analysis_BodyKinematics_vel_global.sto']);
+                        obj.BK.Accelerations = Data([folder filesep ...
+                            'Analysis_BodyKinematics_acc_global.sto']);
+                    case 'ID'
+                        obj.ID.JointTorques = Data([folder filesep 'id.sto']);
+                    case 'CMC'
+                        % Not yet implemented - need to see which files need to
+                        % be read in. Will wait until more CMC-based analysis
+                        % is required.
+                end
+            end
+        end
+        
         function result = calculateCoMD(obj)
         % Calculate CoM displacement.
         
@@ -107,59 +155,14 @@ classdef OpenSimResults < handle
         % Return path to folder containing analysis Data.
         %
         % Throws an error if the analysis data has not been computed.
-            if ~obj.Trial.computed.(analysis)
-                error([analysis ' not computed.']);
-            end
-            
-            folder = obj.ResultsPaths.(analysis);
-        end
-        
-        function load(obj, analyses)
-        % Load the data from each analysis in turn.
-        
-            % Support for a single analysis input as a string. 
-            if isa(analyses, 'char')
-                analyses = {analyses};
-            end
-            
-            for i=1:length(analyses)
-            
-                % Check analysis is computed - if so get results folder.
-                analysis = analyses{i};
-                folder = obj.preload(analysis);
-                obj.(analysis) = {};
-            
-                % Load analysis Data. 
-                switch analysis
-                    case 'GRF'
-                        obj.GRF.Forces = Data(obj.Trial.grfs_path);
-                    case 'IK'
-                        obj.IK.Kinematics = Data([folder filesep 'ik.mot']);
-                        obj.IK.InputMarkers = Data(obj.Trial.input_coordinates);
-                        obj.IK.OutputMarkers = Data(...
-                            [folder filesep 'ik_model_marker_locations.sto']);
-                    case 'RRA'
-                        obj.RRA.Kinematics = ...
-                            Data([folder filesep 'RRA_Kinematics_q.sto']);
-                        obj.RRA.Forces = ...
-                            Data([folder filesep 'RRA_Actuation_force.sto']);
-                        obj.RRA.TrackingErrors = ...
-                            Data([folder filesep 'RRA_Kinematics_q.sto']);
-                    case 'BK'
-                        obj.BK.Positions = Data([folder filesep ...
-                            'Analysis_BodyKinematics_pos_global.sto']);
-                        obj.BK.Velocities = Data([folder filesep ...
-                            'Analysis_BodyKinematics_vel_global.sto']);
-                        obj.BK.Accelerations = Data([folder filesep ...
-                            'Analysis_BodyKinematics_acc_global.sto']);
-                    case 'ID'
-                        obj.ID.JointTorques = Data([folder filesep 'id.sto']);
-                    case 'CMC'
-                        % Not yet implemented - need to see which files need to
-                        % be read in. Will wait until more CMC-based analysis
-                        % is required.
+            folder = [];
+            if ~strcmp(analysis, 'GRF')
+                if ~obj.Trial.computed.(analysis)
+                    error([analysis ' not computed.']);
                 end
+                folder = obj.ResultsPaths.(analysis);
             end
+            
         end
         
         function label = identifyLeadingFoot(obj)
