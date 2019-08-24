6fb40a581ddd9d0a6c3670457dbc1c54323a12e0
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-08-24 21:14:50 +0100

    Temporary reporting step

--- Source/OpenSimTrial.m
+++ Source/OpenSimTrial.m
@@ -119,8 +119,16 @@ classdef OpenSimTrial < handle
                 % Parse inputs.
                 options = obj.parseAnalysisArguments(method, varargin{:});
             
+                if strcmp(method, 'CMC')
+                    tic;
+                end
+                
                 % Run analysis.
                 obj.runTool(method, options);
+                
+                if strcmp(method, 'CMC')
+                    disp(['CMC took ' toc ' seconds.']);
+                end
                 
                 % Update computed status.
                 obj.computed.(method) = true;
