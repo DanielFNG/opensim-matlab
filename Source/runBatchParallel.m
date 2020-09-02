a1536a8517f82f4e2acf0371544d6e3c5d04416d
Daniel Gordon <daniel.gordon@ed.ac.uk>
2020-09-02 15:30:53 +0100

    Update runBatchParallel.m
    This update allows some of the batch to fail, and automatically prunes those from the results.

--- Source/runBatchParallel.m
+++ Source/runBatchParallel.m
@@ -7,16 +7,30 @@ function trials = runBatchParallel(analyses, trials, varargin)
         % Access the specific trial.
         trial = trials{i};
         
-        % Perform analyses.
-        trial.run(analyses, varargin{:});  %#ok<PFBNS>
-        
-        % Assign back to trials array.
-        trials{i} = trial;
+        % Attempt to process the trial. If we fail, store a 0 in the trials
+        % array.
+        try
+            % Perform analyses.
+            trial.run(analyses, varargin{:});  %#ok<PFBNS>
+
+            % Assign back to trials array.
+            trials{i} = trial;
+        catch
+            % A message 
+            trials{i} = 0;
+        end
         
         % Note: the above strategy of setting a local trial{i}, then
         % re-assigning, rather than using trials{i}.run(...), is necessary
         % to actually save the data in trials. A quirk of parfor. 
         
+    end
+    
+    % Prune the trials which failed to process correctly.
+    for i = length(trials):-1:1
+        if trials{i} == 0
+            trials(i) = [];
+        end
     end
 
 end
